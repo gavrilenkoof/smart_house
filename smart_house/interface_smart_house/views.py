addd5885959d9a20030b5058e029939b8c3f3427
gavrilenkoof <vanche10@mail.ru>
2022-11-20 15:06:40 +0300

    add re_path. Add new answers with variables in views

--- smart_house/interface_smart_house/views.py
+++ smart_house/interface_smart_house/views.py
@@ -5,7 +5,21 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("HELLO")
+    host = request.META["HTTP_HOST"]
+    user_agent = request.META["HTTP_USER_AGENT"]
+    user_address = request.META["REMOTE_ADDR"]
+    path = request.path
 
-def about(request):
-    return HttpResponse("ABOUT")
+    return HttpResponse(f"""
+            <p> HOST: {host} </p>
+            <p> user_agent: {user_agent} </p>
+            <p> user_address: {user_address} </p>
+            <p> path: {path} </p>
+    """, headers={"SecretCode": "21234567"})
+
+def about(request, name, age):
+    return HttpResponse(f"""
+            <h2> About user </h2>
+            <p> name: {name} </p>
+            <p> age: {age} </p>
+            """)
